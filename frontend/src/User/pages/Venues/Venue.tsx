7d733ecdd8a53bb86e68275db91ae0493451210b
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-18 17:44:18 +0630

    feat: created venue card

--- frontend/src/User/pages/Venues/Venue.tsx
+++ frontend/src/User/pages/Venues/Venue.tsx
@@ -1,5 +1,45 @@
+import SampleVenueBg from "@/User/assets/sample-venue.png";
+import { House, MapPin, Users } from "lucide-react";
+
 const Venue = () => {
-  return <div>Venue</div>;
+  return (
+    <div className="h-[29rem] w-[26rem] overflow-hidden rounded-lg bg-[#071739] font-['figtree'] text-white">
+      <div className="h-1/2 w-full">
+        <img
+          src={SampleVenueBg}
+          alt="Venue Overview Photo"
+          className="h-full w-full object-cover transition-transform duration-700 hover:scale-105"
+        />
+      </div>
+      {/* Venue Detail Overview */}
+      <div className="p-5">
+        <div className="">
+          <p className="mb-2.5 text-2xl font-bold uppercase">
+            Lotte Hotel Yangon
+          </p>
+          <div className="grid grid-cols-2 grid-cols-[24px_1fr] gap-2">
+            <House />
+            <p>Sapphire Ballroom</p>
+            <Users />
+            <p>800 guests</p>
+            <MapPin />
+            <p>
+              2F, No. 82, Sin Phyu Shin Avenue, Pyay Road, Hlaing Township,
+              Yangon, Myanmar
+            </p>
+          </div>
+        </div>
+        <div className="mt-3 w-full">
+          <a
+            className="float-right cursor-pointer rounded bg-white px-2.5 py-1.5 text-black transition-colors duration-300 hover:bg-[#103263] hover:text-white"
+            href="#"
+          >
+            View Details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Venue;
--- frontend/src/User/pages/Venues/VenueRootLayout.tsx
+++ frontend/src/User/pages/Venues/VenueRootLayout.tsx
@@ -3,8 +3,15 @@ import { Outlet } from "react-router-dom";
 const VenueRootLayout = () => {
   return (
     <>
-      <div>VenueRootLayout</div>
-      <Outlet />
+      <div className="px-16 py-8">
+        <div className="flex w-full flex-col items-center gap-4">
+          <h1 className="figtreef text-[2rem] font-bold">
+            Venues for Your Next Event
+          </h1>
+          <hr className="w-44 border-2 border-black" />
+        </div>
+        <Outlet />
+      </div>
     </>
   );
 };
