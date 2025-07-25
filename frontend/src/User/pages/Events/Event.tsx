14dc64211909c93e2a60a0367abed165e63aeae7
YaeWintAung <yaewintaung.thedev@gmail.com>
2025-07-25 10:25:57 +0630

    feat: pagination for event

--- frontend/src/User/pages/Events/Event.tsx
+++ frontend/src/User/pages/Events/Event.tsx
@@ -4,6 +4,44 @@ import EventPostList from "@/User/components/Events/EventPostList";
 const Event = () => {
   const [searchParam] = useSearchParams();
   const selectCategory = searchParam.get("cat");
+  const events = [
+    {
+      title: "EDM Festival",
+      location: "Yangon convention center",
+    },
+    {
+      title: "LIve concert night",
+      location: "Sedona hotel Yangon",
+    },
+    {
+      title: "Pool Party",
+      location: "Pan Pacific Hotel Yangon",
+    },
+    {
+      title: "Rock Night Festival",
+      location: "Yangon Park",
+    },
+    {
+      title: "EDM Beach Party",
+      location: "Ngapali",
+    },
+    {
+      title: "Indie Live Sessions",
+      location: "Pan Pacific Hotel Yangon",
+    },
+    {
+      title: "Halloween Costume Bash",
+      location: "Yangon convention center",
+    },
+    {
+      title: "Local Drama Performance",
+      location: "Sedona hotel Yangon",
+    },
+    {
+      title: "Jazz & Blues Evening",
+      location: "Pan Pacific Hotel Yangon",
+    },
+  ];
   return (
     <div className="px-5 pt-10">
       <div className="py-5">
@@ -27,8 +65,8 @@ const Event = () => {
             </ul>
           </li>
         </div>
-        <div className="px-10">
-          <EventPostList />
+        <div className="lg:px-10">
+          <EventPostList events={events} eventPerPage={9} />
         </div>
       </div>
     </div>
