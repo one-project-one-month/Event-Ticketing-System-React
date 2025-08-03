cf78cea5796e6e5e971c806a3301c06eaa55b87e
Yu Nilar Thinn <yunilarthinn97@gmail.com>
2025-08-04 01:03:14 +0630

    feat: QrCheck Api Binding

---
+++ frontend/src/User/DataTypes/QrCheck.ts
@@ -0,0 +1,22 @@
+import {type ApiResponse } from "@/types/ApiResponse";
+
+export interface QRInfo {
+  qrString: string;
+  eventName: string;
+  eventCode: string;
+  eventdate: string;
+  eventTimeFrom: string;
+  eventTimeTo: string;
+  gateOpenTime: string;
+  ticketCode: string;
+  ticketPrice: string;
+  ticketType: string;
+  fullName: string;
+  email: string;
+  location: string;
+  address: string;
+}
+
+export type QrInfoByQrCode = ApiResponse<{
+  qrinfo: QRInfo;
+}>;
