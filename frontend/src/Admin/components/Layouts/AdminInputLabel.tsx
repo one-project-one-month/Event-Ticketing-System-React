98d12359faf9a66ce3e73eb6b28a7d2856e442eb
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-08-04 18:32:02 +0630

    feat: textarea option added

--- frontend/src/Admin/components/Layouts/AdminInputLabel.tsx
+++ frontend/src/Admin/components/Layouts/AdminInputLabel.tsx
@@ -15,16 +15,30 @@ export default function AdminInputLabel({
       <label className={`text-xl text-[#615CB8]`} htmlFor={name}>
         {label} {required && <span className={`text-red-400`}>*</span>}
       </label>
-      <input
-        value={value}
-        name={name}
-        type={type}
-        readOnly={readonly}
-        onChange={(e) => onChange(e.target.value)}
-        required={required}
-        placeholder={placeholder}
-        className={`w-96 rounded-[0.5rem] border px-3 py-2 text-lg`}
-      />
+
+      {type === "textarea" ? (
+        <textarea
+          value={value}
+          name={name}
+          readOnly={readonly}
+          onChange={(e) => onChange(e.target.value)}
+          required={required}
+          placeholder={placeholder}
+          className={`w-96 resize-none rounded-[0.5rem] border px-3 py-2 text-lg`}
+          rows={5}
+        />
+      ) : (
+        <input
+          value={value}
+          name={name}
+          type={type}
+          readOnly={readonly}
+          onChange={(e) => onChange(e.target.value)}
+          required={required}
+          placeholder={placeholder}
+          className={`w-96 rounded-[0.5rem] border px-3 py-2 text-lg`}
+        />
+      )}
     </div>
   );
 }
