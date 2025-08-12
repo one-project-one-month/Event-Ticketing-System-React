c971f62249778bb07e78bec898acb296d00d17e7
Yu Nilar Thinn <yunilarthinn97@gmail.com>
2025-08-12 22:36:12 +0630

    feat: logout api binding completed

--- frontend/src/Admin/pages/Auth/LogoutConfirm.tsx
+++ frontend/src/Admin/pages/Auth/LogoutConfirm.tsx
@@ -1,20 +1,28 @@
-import { Button } from "@/User/components/ui/button";
 import {
   Dialog,
   DialogClose,
   DialogContent,
   DialogTrigger,
+  DialogTitle,
+  DialogDescription,
 } from "@/User/components/ui/dialog";
-
+import { Button } from "@/User/components/ui/button";
+import { useAdminAuth } from "@/Admin/data/AdminAuth";
 import LogoutIcon from "@/Admin/data/Icons/logout.svg";
-
 export default function LogoutConfirmation({
   onConfirm,
   children,
 }: {
-  onConfirm: () => void;
+  onConfirm?: () => void;
   children: React.ReactNode;
 }) {
+  const { logout } = useAdminAuth();
+
+  const handleLogout = () => {
+    logout();
+    if (onConfirm) onConfirm();
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -25,27 +33,35 @@ export default function LogoutConfirmation({
         <div className="flex items-center justify-center">
           <img src={LogoutIcon} alt="logouticon" />
         </div>
-        <h2 className="text-2xl font-medium text-white">
-          Are you sure you want to log out?
-        </h2>
-        <p className="mt-2 text-gray-200">
-          You can always log back in at any time.
-        </p>
+
+        <DialogTitle asChild>
+          <h2 className="text-2xl font-medium text-white">
+            Are you sure you want to log out?
+          </h2>
+        </DialogTitle>
+
+        <DialogDescription asChild>
+          <p className="mt-2 text-gray-200">
+            You can always log back in at any time.
+          </p>
+        </DialogDescription>
+
         <div className="mt-6 flex justify-between gap-5">
           <DialogClose asChild>
             <Button variant="outline" size="lg" className="dark:bg-gray-700">
               Cancel
             </Button>
           </DialogClose>
-
-          <Button
-            variant="destructive"
-            size="lg"
-            onClick={onConfirm}
-            className="dark:bg-red-500"
-          >
-            Logout
-          </Button>
+          <DialogClose asChild>
+            <Button
+              variant="destructive"
+              size="lg"
+              onClick={handleLogout}
+              className="dark:bg-red-500"
+            >
+              Logout
+            </Button>
+          </DialogClose>
         </div>
       </DialogContent>
     </Dialog>
